0055dcf8290d5ff2afcc9dfaf891a80ecb5b3eee
Michel Capar-Mleczko <michel.caparmleczko@gmail.com>
2020-05-13 15:45:44 +0200

    DB Create
    Création de la base de données

---
+++ src/Entity/BeerScoreComment.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BeerScoreCommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BeerScoreCommentRepository::class)
+ */
+class BeerScoreComment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Beer::class, inversedBy="Id")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $beer;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getBeer(): ?Beer
+    {
+        return $this->beer;
+    }
+
+    public function setBeer(?Beer $beer): self
+    {
+        $this->beer = $beer;
+
+        return $this;
+    }
+}
