405eb28a32350fc1f5bb3aab2958b3bc3b7b0541
batcow57 <giacomoni.alexandre@hotmail.fr>
2020-05-15 14:23:52 +0200

    resolve conflct

--- src/Controller/BeerController.php
+++ src/Controller/BeerController.php
@@ -8,15 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Beer;
-<<<<<<< HEAD
-use App\Repository\BeerRepository;
 
-=======
 use App\Form\BeerType;
 use App\Repository\BeerRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
->>>>>>> master
+
 
 /**
  * @Route("/beer", name="beer_")
