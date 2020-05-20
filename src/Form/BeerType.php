f9e611e1f4c4077e4bb5d3b784c4c39750356d54
Michel Capar-Mleczko <michel.caparmleczko@gmail.com>
2020-05-20 23:50:04 +0200

    Form updates

--- src/Form/BeerType.php
+++ src/Form/BeerType.php
@@ -3,10 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Beer;
+use App\Entity\Category;
+use App\Form\ImageType;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class BeerType extends AbstractType
 {
@@ -18,6 +24,24 @@ class BeerType extends AbstractType
             ->add('origin')
             ->add('alcohol')
             ->add('brewing_type')
+
+
+            ->add('image', ImageType::class, ['label' => false])
+            
+
+            ->add('category', EntityType::class, [
+                  'label' => 'beer.category',
+                  'class' => Category::class,
+                  'multiple' => false,
+                  'expanded' => true,
+                  'query_builder' => function (EntityRepository $er) {
+                        // Modifie la requête d'affichage de la liste des catégories
+                        return $er->createQueryBuilder('c')
+                        ->orderBy('c.name', 'asc')
+                        ;
+                        }
+            ])
+
             // Ajout du submit
             ->add('save', SubmitType::class);
     }
